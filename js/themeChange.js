b95e6a717af45cbc49684e791ccd5dcd20cd7966
joshkohanek <160525119+joshkohanek@users.noreply.github.com>
2024-08-17 15:30:51 -0500

    Uppercase CSS

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,9 +1,9 @@
 $(".themeChange").on("click", function () {
     if ($(this).hasClass("light-mode")) {
-        $("#stylesheet").attr("href", "./css/style.css")
+        $("#stylesheet").attr("href", "./CSS/style.css")
     }
     else if ($(this).hasClass("dark-mode")) {
-        $("#stylesheet").attr("href", "./css/darkmode.css") 
+        $("#stylesheet").attr("href", "./CSS/darkmode.css") 
 
     } 
     console.log($("#stylesheet").attr("href"));
